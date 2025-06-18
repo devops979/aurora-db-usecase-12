5025f3c0ed17d4240c1b9d493676c87c5c0b7d79
devops979 <devops979@gmail.com>
2025-06-18 21:19:54 +0530

    Update outputs.tf

--- modules/aurora/outputs.tf
+++ modules/aurora/outputs.tf
@@ -8,7 +8,13 @@ output "aurora_reader_endpoint" {
   value       = aws_rds_cluster.aurora.reader_endpoint
 }
 
-output "aurora_secret_arn" {
-  description = "ARN of the Secrets Manager secret"
-  value       = aws_secretsmanager_secret.aurora_credentials.arn
+#output "aurora_secret_arn" {
+#  description = "ARN of the Secrets Manager secret"
+#  value       = aws_secretsmanager_secret.aurora_credentials.arn
+#}
+
+# Output to get the ARN of the secret where AWS stores the master password
+output "aurora_master_secret_arn" {
+  value = aws_rds_cluster.aurora.master_user_secret[0].secret_arn
+  description = "ARN of the secret storing the Aurora master password"
 }
